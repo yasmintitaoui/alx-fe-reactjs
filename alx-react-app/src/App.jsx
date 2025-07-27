6a206a1d624082bd03737bc800c668f22592e48c
Yasmine TITAOUI <yasmintitaoui@gmail.com>
2025-07-28 00:37:54 +0100

    Added Header, MainContent, and Footer components to App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,15 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* Add the WelcomeMessage component here */}
-      <WelcomeMessage />
 
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
