5fe50b0c455f66f87f936cd3ae4c85f94e360885
Yasmine TITAOUI <yasmintitaoui@gmail.com>
2025-07-28 00:42:41 +0100

    Added UserProfile component using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 
 function App() {
@@ -15,8 +17,9 @@ function App() {
     <>
 
       <WelcomeMessage />
-      <Header />
+     <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
