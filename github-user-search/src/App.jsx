8e5ded254142f895e114a42cf62065a9c6657dc2
Yasmine TITAOUI <yasmintitaoui@gmail.com>
2025-07-31 00:21:26 +0100

    Initial GitHub User Search app setup with Axios and folder structure

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search Application</h1>
+      <p>Setup complete. Ready to build.</p>
+    </div>
+  );
+}
+
+export default App;
