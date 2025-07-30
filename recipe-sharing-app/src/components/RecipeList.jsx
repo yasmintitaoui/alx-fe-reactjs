067d752d9920591660713ce4542c62f61a883e02
Yasmine TITAOUI <yasmintitaoui@gmail.com>
2025-07-30 19:49:29 +0100

    Initialize Recipe Sharing App with Zustand state management

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,18 @@
+import useRecipeStore from '../recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id} style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
