c11875f60fac49862a2d6f760465fe1c3282d5be
Yasmine TITAOUI <yasmintitaoui@gmail.com>
2025-07-31 00:57:07 +0100

    Task 1: Add GitHub API search with Search component and service

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,7 +20,7 @@ export default function Search() {
       setUser(data);
     } catch (err) {
       console.error(err);
-      setError('Looks like we can’t find the user');
+      setError('Looks like we cant find the user');
     } finally {
       setLoading(false);
     }
