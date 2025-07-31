79feb61ef63ea8c336d40c8e6f6a2e008844ef0a
Yasmine TITAOUI <yasmintitaoui@gmail.com>
2025-07-31 01:46:46 +0100

    Fix: add advanced search GitHub API integration

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com';
+const BASE_URL = 'https://api.github.com/search/users?q';
 
 export async function fetchUsersByAdvancedSearch(username, location, minRepos) {
   // Build the search query string with filters
