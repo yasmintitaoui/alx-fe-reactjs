b8a7621f7e82ccad2f9cbe8ee98d02aa3f1acc38
Yasmin Titaoui <yasmintitaoui@gmail.com>
2025-08-21 23:52:22 +0100

    Task: Advanced routing setup with Profile, nested routes, and ProfileDetails/ProfileSettings

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,15 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 import ProfileDetails from "./components/ProfileDetails";
 import ProfileSettings from "./components/ProfileSettings";
+import BlogPost from "./components/BlogPost"; 
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Nested profile routes */}
         <Route path="/profile" element={<Profile />}>
           <Route path="details" element={<ProfileDetails />} />
           <Route path="settings" element={<ProfileSettings />} />
         </Route>
+
+        {/* Dynamic blog post route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
